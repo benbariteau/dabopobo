8759c76abe18f18d71eb8abb26a86bb8a8ea2e44
Ben Bariteau <firba1@gmail.com>
2014-11-17 15:30:21 -0800

    Switched to type handler instead of func

--- dabopobo.go
+++ dabopobo.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 )
 
+type karmas map[string]int
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
-		w.Write([]byte("fart"))
-	})
+	k := karmas(make(map[string]int))
+	http.Handle("/", k)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func (k karmas) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	w.Write([]byte("fart"))
 }
