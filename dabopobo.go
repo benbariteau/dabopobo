70f667a0561bcf320974185ebf1e2876d3fd7853
Ben Bariteau <firba1@gmail.com>
2014-11-18 15:31:41 -0800

    Now caseinsensitive (canoncalized as lower)

--- dabopobo.go
+++ dabopobo.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/xuyu/goredis"
 )
@@ -52,42 +53,48 @@ func (s state) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	indentifierMatches := indentifierRegex.FindAllStringSubmatch(text, -1)
 	karma := getkarma.FindStringSubmatch(text)
 	username := r.Form.Get("user_name")
-	if indentifierMatches != nil && username != "slackbot" {
-		for _, match := range indentifierMatches {
-			key := match[1]
-			op := match[2]
-			if key != "" && key != username {
-				var err error
-				switch op {
-				case "--":
-					_, err = s.redis.Incr(key + "--")
-				case "++":
-					_, err = s.redis.Incr(key + "++")
-				case "-+", "+-":
-					_, err = s.redis.Incr(key + "+-")
-				}
-				fmt.Fprintln(os.Stderr, err)
-				if err != nil {
-					panic(err)
-				}
-			}
-		}
-	} else if karma != nil {
+	if karma != nil {
 		name := karma[1]
 		fmt.Println("asking for", name)
 		res := make(map[string]string)
 		karmaset := s.getKarmaSet(name)
+		fmt.Println(karmaset)
 		res["text"] = fmt.Sprintf("%v's karma is %v %v", name, karmaset.value(), karmaset)
 		res["parse"] = "full"
 		res["username"] = "dabopobo"
-		resp, _ := json.Marshal(res)
+		resp, err := json.Marshal(res)
+		if err != nil {
+			panic(err)
+		}
 		fmt.Println(string(resp))
 		w.WriteHeader(200)
 		w.Write(resp)
+	} else if indentifierMatches != nil && username != "slackbot" {
+		for _, match := range indentifierMatches {
+			key := match[1]
+			op := match[2]
+			if key != "" && key != username {
+				suffix := canonicalizeSuffix(op)
+				_, err := s.redis.Incr(strings.ToLower(key) + suffix)
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+	}
+}
+
+func canonicalizeSuffix(suffix string) string {
+	switch suffix {
+	case "--", "++", "+-":
+		return suffix
+	case "-+":
+		return "+-"
+	default:
+		return suffix
 	}
 }
 
 func (s state) getKarmaSet(name string) (k karmaSet) {
+	name = strings.ToLower(name)
 	k.plusplus = getRedisInt(s.redis, name+"++", 0)
 	k.minusminus = getRedisInt(s.redis, name+"--", 0)
 	k.plusminus = getRedisInt(s.redis, name+"+-", 0)
